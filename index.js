b07c210d80da103588e9b00e72ed5ab9e5448241
Aquilav_7 <200577021@student.georgianc.on.ca>
2025-03-13 22:42:08 -0400

    Update on index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const routes = require('./routes/routes');
+const express = require('express');
+const app = express();
+const verifyToken = require('./middleware/auth');
+const db = require('./database/db');
+
+require('dotenv').config();
+db.connect();
+
+app.use(express.json());
+
+
+app.use('/api',routes);
+
+const { API_PORT} = process.env;
+const port = process.env.PORT || API_PORT;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
